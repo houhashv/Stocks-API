7e4ce905413e75366706a84b0c4a8fe4fbfbb933
houhashv <yossihohashvili1@gmail.com>
2018-03-06 00:43:57 +0200

    1

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+      name='yosstheboss',
+      packages=['stocks'],
+      version='0.1',
+      description='Clean and simple Instagram API for Python 3.x'
+)
